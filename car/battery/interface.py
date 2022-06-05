913e342f74832462d5ad4dfdece6f16064f81fe3
KatIsCoding <fabrigoru@gmail.com>
2022-06-05 04:13:08 -0600

    Make all batteries accept current date

--- car/battery/interface.py
+++ car/battery/interface.py
@@ -1,7 +1,6 @@
 import abc
 from datetime import datetime
 class Battery(metaclass=abc.ABCMeta):
-    current_date = datetime.now().date()
     @abc.abstractmethod
     def needs_service(self):
         pass
